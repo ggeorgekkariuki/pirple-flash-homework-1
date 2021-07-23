b536e9ec5fe7ccd9ac7a588f69d6f72ed88fb353
ggeorgekkariuki <gconsummer.gk@gmail.com>
2021-07-23 21:21:59 +0300

    First Commit on Homework 3

--- index.py
+++ index.py
@@ -1,14 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# The Static Pages
 @app.route('/', methods=['GET'])
 def home():
-	return ('Hello World')
+	return render_template('index.html')
 
 @app.route('/about', methods=['GET'])
 def about():
 	return render_template("about.html")
 
+@app.route('/termsOfUse', methods=['GET'])
+def terms():
+	return render_template("terms_of_use.html")
+
+@app.route('/privacy', methods=['GET'])
+def privacy():
+	return render_template("privacy.html")
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+	return render_template("dashboard.html")
+
+# The Authentication Routes
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	if request.method == ['POST']:
+		return
+	return render_template("signup.html")
+	
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == ['POST']:
+		return
+	return render_template("login.html")
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(port=4000, debug=True)
